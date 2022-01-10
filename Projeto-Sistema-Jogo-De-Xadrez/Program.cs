5a3f28684fbd664c2d7351ac6c8e4054a09bee4c
unknown <paulo.eleison@gmail.com>
2022-01-09 22:13:46 -0300

    Corpo da classe Program criado

--- Projeto-Sistema-Jogo-De-Xadrez/Program.cs
+++ Projeto-Sistema-Jogo-De-Xadrez/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+}

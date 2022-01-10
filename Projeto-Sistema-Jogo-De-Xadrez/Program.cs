17bec2ca2bcd79e973e204764efce6332326f5ff
Paulo30032001 <paulo.eleison@gmail.com>
2022-01-10 15:56:53 -0300

    Comecando a colocar pecas no tabuleiro (inserido metodo colocar peca na classe tabuleiro(esse metodo defini a posicao da peca), modificado a classe peca seu construtor defini a peca como null na hora da sua instaciacao pois quem defini a sua posicao é o tabuleiro , Criada a pasta xadrez e suas pecas especificas

--- Projeto-Sistema-Jogo-De-Xadrez/Program.cs
+++ Projeto-Sistema-Jogo-De-Xadrez/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using tabuleiro;
-
+using xadrez;
 namespace xadrez_Console
 {
     class Program
@@ -10,6 +10,32 @@ namespace xadrez_Console
             // Posicao posicao = new Posicao(3, 4);
             Tabuleiro tab = new Tabuleiro(8, 8);
             //   Console.WriteLine(posicao);
+
+            Console.WriteLine("Digite quantas Pecas Deseja inserir");
+            int n = int.Parse(Console.ReadLine());
+            for(int i=1;i<=n; i++)
+            {
+                Console.WriteLine("Digite a linha que deseja inserir");
+                int linha = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite a coluna");
+                int coluna=int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite a inicial da peca que deseja inserir");
+                string opc = Console.ReadLine();
+                if (opc.Equals("T"))
+                {
+                    Posicao posicao = new Posicao(linha, coluna);
+                    Torre t = new Torre(tab, Enum.Parse<Cor>("Preta"));
+                    tab.colocarPeca(t, posicao);
+                }
+                else if (opc.Equals("R"))
+                {
+                    Posicao posicao = new Posicao(linha, coluna);
+                    Torre t = new Torre(tab, Enum.Parse<Cor>("Preta"));
+                    tab.colocarPeca(t, posicao);
+                }
+            }
 
             Tela.imprimirTabuleiro(tab);
 
--- Projeto-Sistema-Jogo-De-Xadrez/tabuleiro/Peca.cs
+++ Projeto-Sistema-Jogo-De-Xadrez/tabuleiro/Peca.cs
@@ -14,15 +14,14 @@ namespace tabuleiro
 
         public Peca() { }
 
-        public Peca(Posicao posicao,Tabuleiro tab,Cor cor)
+        public Peca(Tabuleiro tab,Cor cor)
         {
-            this.posicao = posicao;
+            this.posicao = null;
             this.tab = tab;
             this.cor = cor;
             this.qteMovimentos = 0;
         }
-
-
+      
 
 
 
